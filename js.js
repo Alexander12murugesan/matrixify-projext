2ef79791d5b024e9dee2c7b3aeb40d5c40b08091
Alexander12murugesan <140096948+Alexander12murugesan@users.noreply.github.com>
2023-10-22 19:22:29 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+const fname=document.getElementById("first name");
+const lname=document.getElementById("last name");
+const email=document.getElementById("email");
+const phon=document.getElementById("phone number");
+const mess=document.getElementById("message");
+const submit=document.getElementsByClassName("form contact")[0];
+ 
+
